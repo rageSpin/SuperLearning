8c831c87606437cc8ac8e18ac4796ff799396cb2
rageSpin <stefanokx97@gmail.com>
2023-08-04 10:12:04 +0200

    home page created

---
+++ EtiCos/Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="SuperLearning",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "### This is SuperLearning. A small collection of Giannini Stefano's university notes."
+    }
+)
